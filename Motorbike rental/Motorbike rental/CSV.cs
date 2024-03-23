493f3209378c900d74d8e95d72464a29fc2b1ce0
PATHAPHON <patapol.eing@gmail.com>
2024-03-23 11:00:02 +0700

    add data

--- Motorbike rental/Motorbike rental/CSV.cs
+++ Motorbike rental/Motorbike rental/CSV.cs
@@ -10,6 +10,13 @@ namespace Motorbike_rental
     {
         private string path = @"C:\Users\patap\Documents\final_project-main\final_project-main\Motorbike rental\Data.csv";
 
+        ///try: เป็นบล็อกโค้ดที่ใช้สำหรับ การลองทำ สิ่งใดสิ่งหนึ่ง
+         /// ในกรณีนี้ โค้ดจะพยายาม เขียนข้อความ ไปยังไฟล์
+        /// <summary>
+        /// try: เป็นบล็อกโค้ดที่ใช้สำหรับ การลองทำ สิ่งใดสิ่งหนึ่ง
+        /// </summary>ณีนี้ โค้ดจะ จับข้อผิดพลาด ที่อาจเกิดขึ้นจากการเขียนไฟล์
+        ///โค้ดจะ ส่งคืนค่า false
+
         public bool SavetoFile(string data)
         {
             try
